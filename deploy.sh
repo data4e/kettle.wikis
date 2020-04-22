35fd27de5aa5ac25baee8d5e0f9727bb017fdd17
刘文龙 <1@gitor.org>
2020-04-22 09:44:43 +0800

    修改站点

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -n "$1" ]]; then
+
+	git pull
+	vuepress build wiki/
+
+	git add .
+	git commit -m "$1"
+	git push
+
+	git checkout web-site
+	rm -rf -y docs
+	git checkout master docs/
+	cp -r docs/* ./
+	rm -rf docs
+
+	git add .
+	git commit -m "$1"
+	git push origin web-site
+
+	git checkout master
+else
+	echo "请填写提交信息... 例如 ./deploy.sh commmit mesage"
+fi
---
+++ dev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+vuepress dev wiki
